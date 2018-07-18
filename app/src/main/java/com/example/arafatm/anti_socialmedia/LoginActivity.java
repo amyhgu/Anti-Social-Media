427f5097a9774a72d628d50c9726ff1e5e9c0228
Alison Shikada <ashikada@fb.com>
2018-07-18 16:22:39 -0700

    amy and I both did login

--- app/src/main/java/com/example/arafatm/anti_socialmedia/LoginActivity.java
+++ app/src/main/java/com/example/arafatm/anti_socialmedia/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.arafatm.anti_socialmedia;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -18,6 +20,7 @@ import java.util.Arrays;
 public class LoginActivity extends AppCompatActivity {
     CallbackManager callbackManager;
     LoginButton loginButton;
+    private Button signUpButton;
     private static final String FRIENDS = "user_friends";
 
     @Override
@@ -59,6 +62,15 @@ public class LoginActivity extends AppCompatActivity {
                 exception.printStackTrace();
             }
         });
+
+        signUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+            }
+        });
+
     }
 
     // Every activity/fragment with FacebookSDK Login should forward onActivityResult to the callbackManager.
