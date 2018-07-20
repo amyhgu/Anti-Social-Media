bfe5d0af8329bbeb6209ab9ba88c4a23255fed27
Amy Gu <amygu@fb.com>
2018-07-20 16:46:51 -0700

    changed livequery to just posts

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ var api = new ParseServer({
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   push: pushConfig
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: ["Posts"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
