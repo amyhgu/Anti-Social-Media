1b21b471cf2129d931c54eec1fb974d9e7ceafe8
Amy Gu <amygu@fb.com>
2018-07-27 15:19:45 -0700

    cleaned up groupfeedfragment

--- app/src/main/java/com/example/arafatm/anti_socialmedia/Fragments/GroupFeedFragment.java
+++ app/src/main/java/com/example/arafatm/anti_socialmedia/Fragments/GroupFeedFragment.java
@@ -58,14 +58,8 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
-    private static final String key1 = "111";
 
-    // TODO: Rename and change types of parameters
-    //general feed setup
-    private String mParam1;
-    private String mParam2;
     private String groupObjectId;
-
     private String groupName;
     private int groupId;
     private Group group;
@@ -74,17 +68,7 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
     private ImageView ivGroupPic;
     private ImageView ivStartChat;
     private ImageView ivThreeDots;
-    private ImageView ivCamera;
-    private ImageView ivUpload;
     private ImageView ivLaunchNewPost;
-
-    private File photoFile;
-    public String photoFileName = "photo.jpg";
-    PhotoHelper photoHelper;
-
-    public final String APP_TAG = "MyCustomApp";
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public final static int UPLOAD_IMAGE_ACTIVITY_REQUEST_CODE = 1035;
 
     //posts
 
@@ -154,15 +138,10 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
         super.onViewCreated(view, savedInstanceState);
 
         //creating a post
-        final ParseUser postUser = ParseUser.getCurrentUser();
         final ParseQuery<ParseObject> query = ParseQuery.getQuery("Group");
 
-        messageInput = view.findViewById(R.id.etNewPost);
-        createButton = view.findViewById(R.id.btCreatePost);
         ivStartChat = view.findViewById(R.id.ivStartChat);
         ivThreeDots = view.findViewById(R.id.ivThreeDots);
-        ivCamera = view.findViewById(R.id.ivCamera);
-        ivUpload = view.findViewById(R.id.ivUpload);
         ivLaunchNewPost = view.findViewById(R.id.ivLaunchNewPost);
         rvPosts = view.findViewById(R.id.rvPostsFeed);
 
@@ -202,7 +181,6 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
                     groupName = object.getString("groupName");
                     tvGroupName.setText(groupName);
                     groupId = convert(object.getObjectId());
-                    Log.d("weird", Integer.toString(groupId));
 
                     ivGroupPic = (ImageView) view.findViewById(R.id.ivCoverPhoto);
                     ParseFile groupImage = object.getParseFile("groupImage");
@@ -213,26 +191,6 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
                                 .load(groupImage.getUrl())
                                 .into(ivGroupPic);
                     }
-
-                    ivCamera = view.findViewById(R.id.ivCamera);
-                    ivCamera.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            photoHelper = new PhotoHelper(getContext());
-                            Intent intent = photoHelper.takePhoto();
-                            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-                        }
-                    });
-
-                    ivUpload = view.findViewById(R.id.ivUpload);
-                    ivUpload.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            photoHelper = new PhotoHelper(getContext());
-                            Intent intent = photoHelper.uploadImage();
-                            startActivityForResult(intent, UPLOAD_IMAGE_ACTIVITY_REQUEST_CODE);
-                        }
-                    });
                 } else {
                     e.printStackTrace();
                 }
@@ -241,37 +199,6 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
         });
 
         loadTopPosts();
-
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String message = messageInput.getText().toString();
-
-                createPost(message, postUser, group);
-
-                //TODO: Do i need to create a new instance for groups? This method should require a group
-                //I am trying to pass the group so that the post knows what group it's going under
-
-//                final ParseFile parseFile = new ParseFile(textPost);
-                //debugger says that there's something wrong around here
-                //ParseObject Post that we created
-
-                //save file to parse
-                //this is code from Parsetagram, but probably don't need a Parsefile because we aren't posting with an image
-//                parseFile.saveInBackground(new SaveCallback() {
-//                    @Override
-//                    public void done(ParseException e) {
-//                        if(e == null){
-//                            createPost(message, user);
-//                        }
-//                        else{
-//                            e.printStackTrace();
-//                        }
-//                    }
-//
-//                });
-            }
-        });
 
         ivStartChat.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -294,31 +221,6 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
     public void onDetach() {
         super.onDetach();
         mListener = null;
-    }
-
-    private void createPost(final String message, ParseUser creator, Group group){    // +param -> Parsefile imageFile
-        final Post newPost = new Post();
-        newPost.setMessage(message);
-        newPost.setUser(creator);
-        newPost.setRecipient(group);
-//        newPost.setImage(imageFile);          <== figure out image posting later
-
-        newPost.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e == null) {
-                    Log.d("PostActivity", "Create post success!");
-                    Toast.makeText(GroupFeedFragment.this.getContext(), "Posted!", Toast.LENGTH_LONG).show();
-                    messageInput.setText("");
-                    refreshFeed();
-                }
-                else{
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        // TODO - add the post to the current Group's list/array of Posts
     }
 
     private void loadTopPosts() {
@@ -352,87 +254,6 @@ public class GroupFeedFragment extends Fragment implements CreatePostFragment.On
         adapter.clear();
         loadTopPosts();
         rvPosts.scrollToPosition(0);
-    }
-
-    private void takePhoto() {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference to access to future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.antisocialmedia.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-
-    public void uploadImage() {
-        Intent intent = new Intent(Intent.ACTION_PICK,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-            // Bring up gallery to select a photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), "GroupFeedFragment");
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
-            Log.d("GroupFeedFragment", "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-        return file;
-    }
-
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                if (data != null) {
-                    Uri photoUri = data.getData();
-                    // Do something with the photo based on Uri
-                    Bitmap selectedImage = null;
-                    try {
-                        selectedImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), photoUri);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-                    selectedImage.compress(Bitmap.CompressFormat.PNG, 0, stream);
-                    byte[] Data = stream.toByteArray();
-
-                    Toast.makeText(getContext(), "Picture taken!", Toast.LENGTH_SHORT).show();
-
-                    //TODO
-                    //add to post
-                    //display
-                }
-            } else { // Result was a failure
-                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 
     // for converting group objectId to integer (used for chat channel ID)
